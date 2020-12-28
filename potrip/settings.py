29fc70a8b600a360f8473ecc7c3402f15cf5f183
mengru <4momolmr@gmail.com>
2020-12-28 14:46:28 +0800

    update key

--- potrip/settings.py
+++ potrip/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(c039xzv=@3wrjlj=41wa$ix5&vrmab-=hrw@j(^j(w_k^26hx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
