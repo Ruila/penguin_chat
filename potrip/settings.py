48f9e239bc294635a47f84d3511a9216d1d1e5c8
mengru <4momolmr@gmail.com>
2020-12-29 16:23:15 +0800

    change

--- potrip/settings.py
+++ potrip/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'potrip.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),\\
     }
 }
 
